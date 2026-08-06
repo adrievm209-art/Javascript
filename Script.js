8f153a5f8e4e397b4c7f0b794a4b4fc495ff326b
Adrian <adrianvasquezm209@gmail.com>
2026-08-05 20:35:15 -0500

    listo

--- Script.js
+++ Script.js
@@ -29,14 +29,14 @@
 // console.log(kilos)
 
 // let genero = prompt("¿Eres hombre o mujer?")
-  
+
 // let libras = kilos * 2 
 // console.log(`Pesas ${libras} libras`)
 
 // if(genero === "hombre"){
 
 //  if(libras < 160){
-    
+
 //     console.log("Debes competir en una categoria baja")
 
 //   } else if(libras >= 160 && libras < 180){
@@ -55,7 +55,7 @@
 // if(genero === "mujer"){
 
 //   if(libras < 115){
-    
+
 //     console.log("Debes competir en una categoria femenina baja")
 
 //   } else if(libras >= 115 && libras < 135){
@@ -71,7 +71,7 @@
 
 // }
 
- //metodo de preparacion francesa moka o v60
+//metodo de preparacion francesa moka o v60
 //  //ratio francesa =1/15
 //  //ratio moka =1/14
 //  //ratio v60 =1/16
@@ -79,7 +79,7 @@
 
 //  let metodo = prompt("Elije un metodo: francesa , moka o v60")
 //  let grcafe = prompt("¿Cuantos gramos de cafe utilizaras?")
- 
+
 //  if( metodo === "francesa"){
 
 //   let usa = grcafe * 15
@@ -101,13 +101,79 @@
 
 //  }
 
-let año = prompt("Digina un año en numeros")
-if((año % 400 === 0) || (año % 4 === 0 && año % 100 !== 0)){
+// let año = prompt("Digina un año en numeros")
+// if((año % 400 === 0) || (año % 4 === 0 && año % 100 !== 0)){
 
-    console.log(`El año ${año} es bisiesto`)
+//     console.log(`El año ${año} es bisiesto`)
 
-}else {
+// }else {
 
-    console.log(`El año ${año} no es bisiesto`)
+//     console.log(`El año ${año} no es bisiesto`)
+
+// }
+
+// let cnpersonas = prompt("¿Cuantas personas asistieron a la cena?")
+// let comiomas = prompt("Cual fue la persona que mas consumio?")
+// let cena = prompt("¿Cuanto fue el valor total de la cena ?")
+// let pcomiomas =  ( cena / (cnpersonas + 1) ) 
+// console.log(`${comiomas} debe pagar ${pcomiomas * 2} `)
+
+//let i = 0 ; i<= 10 ; i++
+
+// // let tdm = prompt("Si me dices un numero t dare la tabla correspondiente")
+// // for(let i = 1 ; i <= 10 ; i++){
+
+// // console.log(`${i} * ${tdm} = ${i*tdm}`)
+
+// }
+
+// arrays => Listas
+
+// let frutas = ["Manzana","Pera","Kiwi"]
+// console.log(frutas)
+
+// let precio = 50
+
+// if(precio >= 500){
+//     console.log(`20% de descuento`)
+// } else if(precio >= 300){
+//     console.log(`10% de descuento`)
+// } else{
+//     console.log(`10% de envio`)
+// }
+
+// for (let i = 0; i < 3; i++){
+
+//     let precio = 50
+
+// if(precio >= 500){
+//     console.log(`20% de descuento`)
+// } else if(precio >= 300){
+//     console.log(`10% de descuento`)
+// } else{
+//     console.log(`10% de envio`)
+// }
+
+// }
+
+
+
+
+for (let i = 1; i <= 50; i++) {
+  
+    if(i % 3 === 0){
+
+        console.log(`FIZZBOZZ ${i}`)
+
+    }else if(i % 5 === 0){
+
+         console.log(`BOZZ ${i}`)
+
+    }else if(i % 3 && i % 5){
+
+        console.log(`FIZZ ${i}`)
+
+    }
 
 }
+
